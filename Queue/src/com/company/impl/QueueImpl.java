b85d624bf58f4fdd634b004e5e0131a1b61dc0fe
zx88cvb <295389754@qq.com>
2018-06-22 14:28:06 +0800

    queue stack

---
+++ Queue/src/com/company/impl/QueueImpl.java
@@ -0,0 +1,60 @@
+package com.company.impl;
+
+import com.company.Array;
+import com.company.Queue;
+
+public class QueueImpl<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public QueueImpl(int capacity) {
+        array = new Array<E>(capacity);
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    public QueueImpl() {
+        array = new Array<E>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirts();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: ");
+        res.append("front [");
+        for(int i = 0 ; i < array.getSize() ; i ++){
+            res.append(array.get(i));
+            if(i != array.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+}
---
+++ Stack/src/com/company/leetcode/Solution.java
@@ -0,0 +1,31 @@
+package com.company.leetcode;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack stack = new Stack();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character pop = (Character) stack.pop();
+                if(pop.equals('(') && c != ')'){
+                    return false;
+                }
+                if(pop.equals('{') && c != '}'){
+                    return false;
+                }
+                if(pop.equals('[') && c != ']'){
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
