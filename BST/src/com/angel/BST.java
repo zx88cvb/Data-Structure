bda127e6766dd85246f9a0a0fdf8dfaee268ce3d
zx88cvb <295389754@qq.com>
2019-06-04 14:45:25 +0800

    二分搜索树删除任意一个元素

--- BST/src/com/angel/BST.java
+++ BST/src/com/angel/BST.java
@@ -283,6 +283,64 @@ public class BST<E extends Comparable<E>> {
         return root;
     }
 
+    /**
+     * 从二分搜索树中删除元素为e的节点
+     * @param e 删除的值
+     */
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    /**
+     * 删除以node为根二分搜索树中值为e的节点
+     * @param node 节点
+     * @param e 值
+     * @return 新Node节点
+     */
+    private Node remove(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+
+        // 比较后小于0 说明删除的值在左子数
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else {    // e==node.e
+            // 待删除节点左子树为空情况
+            if (node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空情况
+            if (node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            // 左右子树均不为空
+            // 找到比待删除节点大的最小节点，即待删除节点右子树最小节点
+            // 利用这个节点顶替待删除节点
+
+            // 找到右子树最小节点
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
