18f713cf319ccd9be1b5983875d1e468a6d47f65
zx88cvb <295389754@qq.com>
2019-07-02 15:26:47 +0800

    set

---
+++ Set/src/com/angel/bst/BSTSet.java
@@ -0,0 +1,44 @@
+package com.angel.bst;
+
+import com.angel.Set;
+
+/**
+ * 基于二分搜索树
+ * @Author angel
+ * @Date 19-7-2
+ */
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+    /**
+     * BST二分搜索树
+     */
+    private BST<E> bst;
+
+    public BSTSet() {
+        bst = new BST<>();
+    }
+
+    @Override
+    public void add(E e) {
+        bst.add(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        bst.remove(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return bst.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return bst.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return bst.isEmpty();
+    }
+}
