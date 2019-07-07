4d08ae0940b9ae5483754c6a8e187a5cecec5781
zx88cvb <295389754@qq.com>
2019-07-07 21:14:15 +0800

    leetcode 804

---
+++ Set/src/com/angel/leetcode/Solution.java
@@ -0,0 +1,27 @@
+package com.angel.leetcode;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 804. 唯一摩尔斯密码词
+ * @Author: Angel
+ * @Date: 2019/7/7.
+ * @Description:
+ */
+public class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String [] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> treeSet = new TreeSet<>();
+
+        for (String word: words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                sb.append(codes[word.charAt(i) - 'a']);
+            }
+            treeSet.add(sb.toString());
+        }
+        return treeSet.size();
+    }
+}
